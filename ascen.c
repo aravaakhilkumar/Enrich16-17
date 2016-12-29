9b5ed7eb8a3755fd7a5ad7e6c85b046a025873fa
aravaakhilkumar <akhilagni000@gmail.com>
2016-12-29 16:47:18 +0530

    Create ascen.c

---
+++ ascen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main () 
+{ 
+int i,j,k=0,t,p,a[10],n;
+printf ("enter the number");
+scanf("%d",&n);
+ while(n>0)
+ {
+    r=n%10;
+    n=n/10;
+    a[k]=p;
+    k++;
+ } 
+ for(i=0;i<k;i++) 
+ {
+  for(j=i+1;j<k;j++)
+  {
+  if(a[i]>a[j]) 
+  {
+   t=a[i];
+   a[i]=a[j];
+   a[j]=t;
+  }
+ } 
+ for(i=0;i<k;i++) 
+  {
+  printf("output",a[k]);
+  } 
+}return 0;
+} 
